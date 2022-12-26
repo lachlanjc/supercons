b00e4c00be096a99c3f1a2867dd1a01f3dda10a1
Lachlan Campbell <lachlanjc@hey.com>
2022-12-26 15:57:21 -0500

    Mess with config

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { glyphs, type GlyphName } from './glyphs'
 export { glyphNames } from './glyphs'
 
-type Props<T> = {
-  is?: Function | T | string
+interface Props {
   glyph?: GlyphName
   size?: number
 }
@@ -13,13 +12,13 @@ function Icon<T extends React.ElementType = 'svg'>({
   size = 32,
   glyph = 'like',
   ...props
-}: Props<T> & React.ComponentPropsWithoutRef<T>) {
+}: Props & React.ComponentPropsWithoutRef<T>) {
   return (
     <Component
       fillRule="evenodd"
       clipRule="evenodd"
       strokeLinejoin="round"
-      strokeMiterlimit="1.414"
+      strokeMiterlimit={1.414}
       xmlns="http://www.w3.org/2000/svg"
       aria-label={glyph}
       viewBox="0 0 32 32"
